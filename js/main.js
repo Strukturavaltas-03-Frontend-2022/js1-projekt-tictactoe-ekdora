bd3c083542ceb97361b9c2fd12d943be02889750
ekdora <dora.egrykiss@gmail.com>
2022-08-12 17:39:58 +0200

    Declaring base variables, const initState with for cycles, html body

---
+++ js/main.js
@@ -0,0 +1,204 @@
+const cols = 3;
+const rows = 3;
+const matrix = [];
+let stepCount = 0;
+let mark = "X";
+
+const initState = () => {
+  for (let row = 0; row < rows; row += 1) {
+    const row = [];
+    for (let column = 0; column < cols; column += 1) {
+      row.push(null);
+    }
+    matrix.push(row);
+  }
+  /* matrix = Array.from(Array(rows), () => Array(cols).fill(null)); */
+}
+
+const changeMatrixValue = (element) => {
+  const row = parseInt(element.dataset.row, 10);
+  const cell = parseInt(element.dataset.cell, 10);
+  matrix[row][cell] = element.textContent;
+}
+
+/**
+Fejtsd ki a deleteSigns elnevezésű függvényt, 
+amely kiválasztja az összes cellát, 
+és mindegyik elemben elhelyez egy üres string-et.
+*/
+/* getelementbyclassname(cell) az egy iterálható tömb lesz, 
+azon végig kell iterálni és minden elem text contentjét be kell állígtani üres stringre */
+const deleteSigns = () => {
+
+}
+
+/**
+Fejtsd ki az increaseCounter elnevezésű függvényt, 
+amely a megtett lépések számát növeli eggyel.
+*/
+const increaseCounter = () => {
+
+}
+
+/**
+Fejtsd ki a modifyCell elnevezésű függvényt, 
+amely beállítja az elem tartalmának a használt jelet,
+majd kattintásra (esemény) eltávolítja a handleClick függvényt. 
+*/
+/* element textcontent beállítása és a mark beállítása neki.
+element onclick-en eltávolítani a handelclicket */
+const modifyCell = (element) => {
+
+}
+
+/**
+Fejtsd ki a setMark elnevezésű függvényt, 
+amely a jelre beállítja a következő lépésnél használt jelet úgy, 
+hogy ha az X-et használtuk éppen, azaz a jel === X, 
+akkor beállítja a 0-t, hiszen a másik játékos azzal fog tenni egy jelet,
+ha pedig a 0-t használtuk, beállítja az X-et.
+*/
+const setMark = () => {
+
+}
+
+/**
+Fejtsd ki a handleClick elnevezésű függvényt, 
+amely meghívja a következő függvényeket:
+- increaseCounter()
+- modifyCell(event.target)
+- setMark()
+- changeMatrixValue(event.target)
+- checkWinner()
+*/
+const handleClick = (event) => {
+
+}
+
+/**
+Fejtsd ki az addClickListener elnevezésű függvényt, 
+amely kiválasztja a cellákat, 
+és kattintásra (esemény) mindegyikhez hozzáadja a handleClick függvényt.
+*/
+const addClickListener = () => {
+
+}
+
+/**
+Fejtsd ki a removeAllClickListeners elnevezésű függvényt, 
+amely kiválasztja a cellákat, 
+és kattintásra (esemény) mindegyikről eltávolítja a handleClick függvényt.
+*/
+const removeAllClickListeners = () => {
+
+}
+
+/**
+Fejtsd ki a checkValues elnevezésű függvényt, 
+amely végigmegy a kapott tömb sorain,
+és a sor minden EGYES elemének értéke esetében megvizsgálja, 
+hogy az 0 vagy X. 
+Ha a sor minden egyes eleme 0 vagy X, 
+akkor a 0 vagy az X győzött.
+Ha valaki győzött, akkor pl. egy ilyen tömböt kapunk: [true, false, false]
+*/
+const checkValues = (array) => array.map(row => {/*Ide írd a kódot!*/ })
+  .indexOf(true) !== -1;
+/*
+Ha true-t kapunk visza adott sorra, akkor 
+annak indexét vizsgálva nem kaphatunk -1-et.
+Azaz az elem benne van a tömbben.
+*/
+
+const checkColumnValues = () =>
+  checkValues(matrix.map((array, i) =>
+    array.map((item, j) => matrix[j][i])));
+
+const checkDiagonalValues = () =>
+  checkValues([
+    matrix.map((array, i) => matrix[i][i]),
+    matrix.map((array, i) => matrix[i][matrix[i].length - i - 1])
+
+    /*
+    Miután az első függvénnyel leellenőriztük a sorok tartalmát,
+    a fentiekkel megvizsgáljuk az oszlopok és az átlókban lévő mezők tartalmát.
+    Ez azért fontos, mert így tudhatjuk meg, 
+    hogy lesz-e három azonos jel egymás mellett/alatt/átlósan.
+    */
+  ]);
+
+/*
+Fejtsd ki a checkWinner elnevezésű függvényt, 
+amely...
+*/
+const checkWinner = () => {
+  // ...kiírja a konzolra a checkColumnValues()t és a checkDiagonalValues()-t,
+
+  /*
+  majd meghívja az endGame()-et, ha hogy HA
+  a checkValues(matrix) vagy a checkColumnValues() vagy a checkDiagonalValues() igaz.
+  */
+
+}
+
+/**
+A HTML-ben a játékteret követően vegyél fel egy divet message osztállyal,
+'Let's play.' tartalommal.
+
+Fejtsd ki a setMessage elnevezésű függvényt, 
+amely kiválasztja a message osztályú elemet, 
+és az üzenetet állítja be a div tartalmának.
+*/
+const setMessage = (message) => {
+
+}
+
+/**
+Fejtsd ki a startGame elnevezésű függvényt, 
+amely meghívja a következő függvényeket:
+- initState()
+- addClickListener()
+- newGame()
+*/
+const startGame = () => {
+
+}
+
+/**
+Fejtsd ki az endGame elnevezésű függvényt, 
+amely a setMessage nevű függvény segítségével beállítja az üzenetet, 
+amelynek tartalma:
+'The winner is Player ' 
+plusz: 
+(mark === 'X' ? 'O' : 'X') + '.')
+
+Ez az utóbbi kódrészlet kiválasztja azt a jelet, amellyel a nyertes játszott
+(és egy pontot helyez el a mondat végén).
+
+Ezután a függvény meghívja a removeAllClickListeners() nevű függvényt.
+*/
+const endGame = () => {
+
+}
+
+/*
+Indíts el egy új játékot az alábbi függvény segítségével!
+
+Hozz létre a HTML-ben a játéktéren kívül egy gombot!
+*/
+const newGame = () => {
+  // Válaszd ki a gombot!
+
+  /*
+  Tegyél rá/adj hozzá egy eseményfigyelőt, 
+  amely kattintásra meghívja a következő függvényeket:
+      - initState()
+      - addClickListener()
+      - deleteSigns()
+      - setMessage('Playing...')
+      - setMark()
+      */
+
+}
+
+startGame();
